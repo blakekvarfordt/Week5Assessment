eee45cd3d538c775278ccd59d3927db71e4d3f76
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-08-30 09:42:59 -0600

    Sets up Contact Model with a CKRecord initializer  for cloud kit

---
+++ Week5Assessment/Week5Assessment/ModelController/ContactController.swift
@@ -0,0 +1,9 @@
+//
+//  ContactController.swift
+//  Week5Assessment
+//
+//  Created by Blake kvarfordt on 8/30/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+import Foundation
---
+++ Week5Assessment/Week5Assessment/Views/ContactTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ContactTableViewCell.swift
+//  Week5Assessment
+//
+//  Created by Blake kvarfordt on 8/30/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var contactNameLabel: UILabel!
+    @IBOutlet weak var contactNumberLabel: UILabel!
+    @IBOutlet weak var contactEmailLabel: UILabel!
+    
+
+}
