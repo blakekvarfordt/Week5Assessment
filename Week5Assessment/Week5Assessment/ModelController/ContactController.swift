3039662e76e4356364253f5430cb06622c4b7397
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-08-30 10:27:46 -0600

    Sets up Contact Controller with save function and query function

--- Week5Assessment/Week5Assessment/ModelController/ContactController.swift
+++ Week5Assessment/Week5Assessment/ModelController/ContactController.swift
@@ -7,3 +7,65 @@
 //
 
 import Foundation
+import CloudKit
+
+class ContactController {
+    
+    // Database that is used to save CKRecords. I choose to have a private database becasue each user should have his / her own contacts
+    let database = CKContainer.default().privateCloudDatabase
+    
+    // Shared Instance
+    static let shared = ContactController()
+    
+    // Empty array to fill with CKRecords and Contacts
+    var contacts = [Contact]()
+    
+    // Function to make CKRecords out of our contacts, save them to the database, and append the created contact to the emptyArray
+    func createAndSave(contact: Contact, completion: @escaping (Bool) -> Void) {
+        
+        let contact = contact
+        
+        let contactRecord = CKRecord(contact: contact)
+        
+        database.save(contactRecord) { (record, error) in
+            
+            if let error = error {
+                print("Error saving record in \(#function) \(error) \(error.localizedDescription)")
+                completion(false)
+                return
+            }
+            
+            guard let contactRecord = record, let contact = Contact(ckRecord: contactRecord) else {completion(false); return }
+            
+            self.contacts.append(contact)
+            
+            completion(true)
+            
+        }
+    }
+    
+    // Function to query for CKRecords that we want for our app. Retrieve Contact items from the records and set the empty Array as those Contacts
+    func queryForRecords(completion: @escaping (Bool) -> Void) {
+        
+        let predicate = NSPredicate(value: true)
+        
+        let query = CKQuery(recordType: ContactConstants.recordTypKey, predicate: predicate)
+        
+        database.perform(query, inZoneWith: nil) { (records, error) in
+            
+            if let error = error {
+                print("Error saving record in \(#function) \(error) \(error.localizedDescription)")
+                completion(false)
+                return
+            }
+            
+            guard let records = records else { completion(false); return }
+            
+            let contacts = records.compactMap({Contact(ckRecord: $0)})
+            
+            self.contacts = contacts
+            
+            completion(true)
+        }
+    }
+}
